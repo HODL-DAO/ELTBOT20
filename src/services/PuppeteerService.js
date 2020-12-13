7ac8e7ceae58e0ec670f7855aaf6c195973bd20f
radu-m <radu.miu@gmail.com>
2020-12-13 13:49:30 +0700

    WIP; Functional screenshot as buffer; Broken events on document;

--- src/services/PuppeteerService.js
+++ src/services/PuppeteerService.js
@@ -1,26 +1,74 @@
 import puppeteer from 'puppeteer';
 import ChartsService from './charts';
 
-const htmlString = '<!DOCTYPE html>';
-
 const padding = 15;
-// const url = process.env.URL;
+const url = "https://www.hodldao.xyz";
+
+const AUTO_TAB = true;
 
 async function createHtmlDoc() {
     const browser = await puppeteer.launch({ headless: false })
-    const page = await browser.newPage();
+    const page = (await browser.pages())[0];
+
+    console.log(' browser page ', page);
+
     await page.setViewport({
         width: 600,
         height: 300,
-        deviceScaleFactor: 1
+        deviceScaleFactor: 1,
+    });
+
+    var screenshotNum = 1;
+
+    // inject the chart into the DOM and take a screenshot of it
+    await page.exposeFunction('getElementAsPNG', async selector => {
+        const chartElem = await page.$(selector);
+        console.log(`Taking screenshot of ${selector}`);
+        const boundingBox = await chartElem.boundingBox();
+        console.dir(` bounding box: ${boundingBox}`);
+
+        let scrSht = await chartElem.screenshot({
+            path: `./screenshot_${screenshotNum++}.png`,
+            clip: {
+                x: boundingBox.x - padding,
+                y: boundingBox.y - padding,
+                width: boundingBox.width + padding * 2,
+                height: boundingBox.height + padding * 2,
+            }
+        });
+
+        console.log(' The ScreenShot as stream: ', scrSht);
+
+        return scrSht;
     });
 
     await page.evaluateOnNewDocument(() => {
 
-        console.log(' !!!!!!!!!! 00000 !!!!!!!! ', window);
+        // window.onloadp = async (e) => {
+        //     console.log(' document.body ----- ', document.body);
+        //     const chart = await ChartsService.EltMarketCandles(document.body);
+        //     console.log(' chart ----- ', chart);
+        // };
 
-        ChartsService.EltMarketCandles(document.body);
+        // window.getElementAsPNG(chart.id)
+
+        window.addEventListener('keyup', evt => {
+            console.log(' !!!!!!!!!! keyup evt !!!!!!!! ', evt);
+
+            if (evt.key === 'Tab') {
+                // console.log(' evt ', evt);
+            }
+        });
     });
+
+    console.log(' createHtmlDoc page ', url);
+
+    await page.goto(url);
+
+    // if (AUTO_TAB) {
+    await page.waitForTimeout(1000);
+    await page.keyboard.press('Tab');
+    // }
 };
 
 export default {
