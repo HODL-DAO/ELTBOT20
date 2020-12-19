06aaa6583e6ede16f77d202092290352ef3c7b78
radu-m <radu.miu@gmail.com>
2020-12-19 22:56:57 +0700

    Try express script for heroku deployment

--- src/index.js
+++ src/index.js
@@ -1,22 +1,25 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-const http = require('http'); //loads the library to enable it to act as a server
-var port = process.env.PORT || 5000; //specifies the port no to whatever heroku gives or 5000 on local host
-http.createServer(function (req, res) { // creates a server
-  res.writeHead(200, { 'Content-type': 'text/plain' }); //Specifies that the respones "hello" is a text
-  res.end("hello"); //shows the text "hello" on th eweb page
-}).listen(port); // attaches this server to the port no.
-
 import Telegraf, { Extra, Markup } from "telegraf";
 import { handlers } from "./handlers";
 import mongoose from "mongoose";
 import rateLimit from "telegraf-ratelimit";
 import { attachUser } from "./middleware";
 import {
-  AddressService,
   CacheService
 } from './services';
+
+const express = require('express')
+const expressApp = express()
+
+const port = process.env.PORT || 3000
+expressApp.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+expressApp.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+})
 
 let bot = {};
 if (process.env.IS_PROD === true) {
